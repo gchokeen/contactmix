0b22e6b768e259b482f92d981fb95bf0c10f8a2c
gchokeen <gchokeen@gmail.com>
2014-04-03 09:32:49 +0530

    plugin base file ini

---
+++ contactmix.php
@@ -0,0 +1,183 @@
+<?php
+/**
+ * @package contactmix
+ * @version 0.1
+ */
+/*
+Plugin Name: Contact Mix
+Plugin URI: http://www.example.com
+Description: contactmix desc placeholder
+Author: Gowri Sankar.R
+Version: 0.1
+Author URI: http://www.example.com
+Text Domain: contactmix
+*/
+
+define('CONTACTMIX_PLUGIN_NAME', plugin_basename(__FILE__));
+define('CONTACTMIX_PLUGIN_DIR', dirname(__FILE__) . DIRECTORY_SEPARATOR);
+define('CONTACTMIX_PLUGIN_VERSION','0.1');
+
+
+require_once 'classes/settings.php';
+
+
+if (!class_exists('ContactMix')) {
+
+	class ContactMix{
+		/**
+		 * @var ContactMix
+		 */
+		static private $_instance = null;
+		
+		private $_priceplowapi;
+		
+		private $_priceplowcore;
+		
+		/**
+		 * Get ContactMix object
+		 *
+		 * @return ContactMix
+		 */
+		static public function getInstance()
+		{
+			if (self::$_instance == null) {
+				self::$_instance = new ContactMix();
+			}
+
+			return self::$_instance;
+		}
+
+
+		private function __construct()
+		{
+
+			register_activation_hook(CONTACTMIX_PLUGIN_NAME, array(&$this, 'pluginActivate'));
+			register_deactivation_hook(CONTACTMIX_PLUGIN_NAME, array(&$this, 'pluginDeactivate'));
+			register_uninstall_hook(CONTACTMIX_PLUGIN_NAME, array('contactmix', 'pluginUninstall'));
+
+			
+			## Register plugin widgets
+			add_action('init', array($this, 'load_transl'));
+			add_action('plugins_loaded', array(&$this, 'pluginLoad'));
+
+			add_action( 'widgets_init', array(&$this, 'widgetsRegistration') );
+			
+			if (is_admin()) {
+			add_action('wp_print_scripts', array(&$this, 'adminLoadScripts'));
+			add_action('wp_print_styles', array(&$this, 'adminLoadStyles'));
+			}
+			else{
+
+			add_action('wp_print_scripts', array(&$this, 'siteLoadScripts'));
+			add_action('wp_print_styles', array(&$this, 'siteLoadStyles'));
+
+
+			}
+
+			add_action( 'wp_footer',array(&$this, 'footerScript'));
+
+		}
+
+		public function load_transl()
+		{
+			load_plugin_textdomain('contactmix', FALSE, dirname(plugin_basename(__FILE__)).'/languages/');
+		}
+
+		##
+		## Loading Scripts and Styles
+		##
+	
+		public function adminLoadStyles()
+		{
+            	  
+
+		}
+	
+		public function adminLoadScripts(){
+		  
+		}
+	
+	
+	
+		public function siteLoadStyles(){
+			
+	
+		}
+	
+	
+		public function siteLoadScripts(){
+			wp_enqueue_script( 'jquery' );			  
+		}
+
+
+
+		##
+		## Widgets initializations
+		##
+
+		public function widgetsRegistration(){
+		  		 
+		 		 
+		}
+
+
+		
+		##
+		## Plugin Activation and Deactivation
+		##
+
+		/**
+		* Activate plugin
+		* @return void
+		*/
+		public function pluginActivate()
+		{
+			
+			
+		 
+		}
+
+		/**
+		* Deactivate plugin
+		* @return void
+		*/
+		public function pluginDeactivate(){
+			
+		}
+
+		/**
+		* Uninstall plugin
+		* @return void
+		*/
+		static public function pluginUninstall()
+		{
+
+		}
+
+
+		public function pluginLoad(){
+
+		}
+		
+		public function footerScript(){
+		  
+		  if( wp_script_is( 'jquery', 'done' ) ) {
+		      ?>
+			<!--Contact Mix Script -->
+			<script type="text/javascript" src="//app.contactmix.com/remote/js/contactmix.min.js"></script>
+		    
+		    <?php		    
+		  }
+
+		}	  
+
+		}
+
+}
+
+
+
+//instantiate the class
+if (class_exists('ContactMix')) {
+	$ContactMix =  ContactMix::getInstance();
+}
