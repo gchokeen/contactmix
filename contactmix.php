53317468ca7a2cd9f77f88bb7b9adeef1f6e96b3
gchokeen <gchokeen@gmail.com>
2014-04-14 12:32:58 +0530

    licences added

--- contactmix.php
+++ contactmix.php
@@ -13,6 +13,25 @@ Author URI: http://contactmix.com
 Text Domain: contactmix
 */
 
+/*  
+    Copyright 2012  contactmix  (email : thirdparty@contactmix.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+
 define('CONTACTMIX_PLUGIN_NAME', plugin_basename(__FILE__));
 define('CONTACTMIX_PLUGIN_DIR', dirname(__FILE__) . DIRECTORY_SEPARATOR);
 define('CONTACTMIX_PLUGIN_VERSION','1.0');
